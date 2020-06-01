8e5c8f71b8475634b9df9f941064ed6584a2502d
CarmenBene <camy_marya2001@yahoo.com>
2020-06-01 20:29:53 +0300

    add NumereRationale

---
+++ NumereRationale/NumereRationale/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumereRationale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Q A = new Q(15,25);
+            Q B = new Q(2,8);
+
+            Console.WriteLine("Primul numar este: " + A.ToString());
+            Console.WriteLine("Al doilea numar este: " + B.ToString());
+
+            //Adunare
+            Q C = A + B;
+            Console.WriteLine("Suma celor 2 numere este: " + C.ToString());
+
+            //Scadere
+            Q S = A - B;
+            Console.WriteLine("Diferenta celor 2 numere este: " + S.ToString());
+
+            //Inmultire
+            Q D = A * B;
+            Console.WriteLine("Produsul celor 2 numere este: " + D.ToString());
+
+            //Impartire
+            Q I = A / B;
+            Console.WriteLine("Rezultatul impartirii este: "+I.ToString());
+
+            //Ridicare la putere
+            int power = 4;
+            Q E = Q.POW(A, power);
+            Console.WriteLine("Primul numar la puterea " + power + "este: " + E.ToString());
+
+            int power2 = 2;
+            Q E2 = Q.POW(B, power);
+            Console.WriteLine("Al doilea numar la puterea " + power2 + "este: " + E2.ToString());
+
+
+            Console.WriteLine("A<B?");
+            if (A < B)
+                Console.WriteLine("Da");
+            else
+                Console.WriteLine("Nu");
+
+            Console.WriteLine("A>B?");
+            if (A > B)
+                Console.WriteLine("Da");
+            else
+                Console.WriteLine("Nu");
+
+            Console.WriteLine("A=B?");
+            if (A == B)
+                Console.WriteLine("Da");
+            else
+                Console.WriteLine("Nu");
+
+            Console.WriteLine("A!=B?");
+            if (A != B)
+                Console.WriteLine("Da");
+            else
+                Console.WriteLine("Nu");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ NumereRationale/NumereRationale/Q.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumereRationale
+{
+    public class Q
+    {
+        static int cmmdc(int A, int B)
+        {
+            while (B != 0)
+            {
+                int r = A % B;
+                A = B;
+                B = r;
+            }
+            return A;
+        }
+
+        private int numarator;
+        private int numitor;
+
+        public Q(int numarator, int numitor)
+        {
+            this.numarator = numarator;
+            this.numitor = numitor;
+            this.ireductibil();
+
+        }
+        public Q()
+        {
+        }
+
+        public override string ToString()
+        {
+            return "(" + numarator + "/" + numitor + ")";
+        }
+
+        public static Q operator +(Q A, Q B)
+        {
+            Q tor = new Q();
+            tor.numarator = A.numarator * B.numitor + B.numarator * A.numitor;
+            tor.numitor = A.numitor * B.numitor;
+            tor.ireductibil();
+            return tor;
+        }
+
+        public static Q operator *(Q A, Q B)
+        {
+            Q tor = new Q();
+            tor.numarator = A.numarator * B.numarator;
+            tor.numitor = A.numitor * B.numitor;
+            tor.ireductibil();
+            return tor;
+        }
+
+        public static Q operator -(Q A, Q B)
+        {
+            Q tor = new Q();
+            tor.numarator = A.numarator * B.numitor - B.numarator * A.numitor;
+            tor.numitor = A.numitor * B.numitor;
+            tor.ireductibil();
+            return tor;
+        }
+
+        public static Q operator /(Q A, Q B)
+        {
+            Q tor = new Q();
+            tor.numarator = A.numarator * B.numitor;
+            tor.numitor = A.numitor * B.numarator;
+            tor.ireductibil();
+            return tor;
+        }
+
+        public static Q POW(Q A, int n)
+        {
+            Q tor = new Q();
+            tor.numarator = (int)Math.Pow(A.numarator, n);
+            tor.numitor = (int)Math.Pow(A.numitor, n);
+            tor.ireductibil();
+            return tor;
+        }
+
+        public static bool operator <(Q A, Q B)
+        {
+            if (A.numarator * B.numitor < A.numitor * B.numarator)
+                return true;
+            return false;
+        }
+
+        public static bool operator >(Q A, Q B)
+        {
+            if (!(A < B))
+                return true;
+            return false;
+        }
+
+        public void ireductibil()
+        {
+            int t = cmmdc(numarator, numitor);
+            numarator /= t;
+            numitor /= t;
+        }
+
+        public static bool operator ==(Q A, Q B)
+        {
+            A.ireductibil();
+            B.ireductibil();
+            if (A.numitor == B.numitor && A.numarator == B.numarator)
+                return true;
+            return false;
+        }
+
+        public static bool operator !=(Q A, Q B)
+        {
+            if (!(A == B))
+                return true;
+            return false;
+        }
+
+        public static bool operator <=(Q A, Q B)
+        {
+            if ((A == B) || (A < B))
+                return true;
+            return false;
+        }
+        public static bool operator >=(Q A, Q B)
+        {
+            if (!(A <= B))
+                return true;
+            return false;
+        }
+
+    }
+}
+
