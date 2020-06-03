ee3cec14f0212c3c32f9c3a013a1f3a3d44f590c
CarmenBene <camy_marya2001@yahoo.com>
2020-06-03 23:09:12 +0300

    add interface

---
+++ IForma2D/IForma2D/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IForma2D
+{
+    class Program
+    {
+        public interface IForma2D
+        {
+            void Arie();
+            void LungimeaFrontierei();
+            string denumire { get; }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
