dbf94b9e6a235609f0767dfcca20e54ec047093a
CarmenBene <camy_marya2001@yahoo.com>
2020-06-03 22:19:22 +0300

    add Elev class

---
+++ Clasa_Elevi/Clasa_Elevi/Elev.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clasa_Elevi
+{
+    public class Elev
+    {
+        public string name;
+        string surname;
+        public float media;
+
+        public Elev()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return name + " " + surname + " " + media.ToString("0.00");
+        }
+    }
+}
