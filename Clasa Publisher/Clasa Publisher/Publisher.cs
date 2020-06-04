6901446d174aa0ece0f2a65455fac3c1693fff07
CarmenBene <camy_marya2001@yahoo.com>
2020-06-04 18:02:36 +0300

    Add publisher class

--- Clasa Publisher/Clasa Publisher/Publisher.cs
+++ Clasa Publisher/Clasa Publisher/Publisher.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Clasa_Publisher
 {
+    public delegate void iteratie(int n);
     class Publisher
     {
+        public event iteratie eveniment;
+
+        static void DeclansareEveniment()
+        { 
+
+        }
+
     }
 }
