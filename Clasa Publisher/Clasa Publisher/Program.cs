f686394c1e4860e8469617dea4987827fc4bc92c
CarmenBene <camy_marya2001@yahoo.com>
2020-06-05 19:11:37 +0300

    final commit -Publisher class

--- Clasa Publisher/Clasa Publisher/Program.cs
+++ Clasa Publisher/Clasa Publisher/Program.cs
@@ -10,6 +10,13 @@ namespace Clasa_Publisher
     {
         static void Main(string[] args)
         {
+            Publisher P = new Publisher();
+            SubscriberA A = new SubscriberA();
+            SubscriberB B = new SubscriberB();
+            
+            P.eveniment += new iteratie(A.HandlerA);
+            P.eveniment += new iteratie(B.HandlerB);
+            P.DeclansareEveniment();
 
         }
     }
