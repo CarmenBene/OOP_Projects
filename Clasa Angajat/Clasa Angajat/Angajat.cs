1930d219ad2cc13847fa3c09fe529b78db600a7b
CarmenBene <camy_marya2001@yahoo.com>
2020-06-08 15:34:16 +0300

    add ListaAngajati class; +implementation of angajat class

--- Clasa Angajat/Clasa Angajat/Angajat.cs
+++ Clasa Angajat/Clasa Angajat/Angajat.cs
@@ -8,6 +8,19 @@ namespace Clasa_Angajat
 {
     class Angajat
     {
+        public string nume;
+        public int vechime;
 
+        public Angajat(string data)
+        {
+            string[] local = data.Split(' ');
+            nume = local[0] + " " + local[1];
+            vechime = int.Parse(local[2]);
+        }
+        public Angajat(string nume, int vechime)
+        {
+            this.nume = nume;
+            this.vechime = vechime;
+        }
     }
 }
