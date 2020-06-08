7bb27cf68ba2fd4db7ec62a15cbdb954d91fe857
CarmenBene <camy_marya2001@yahoo.com>
2020-06-08 15:39:48 +0300

    add remove and add methods

--- Clasa Angajat/Clasa Angajat/ListaAngajati.cs
+++ Clasa Angajat/Clasa Angajat/ListaAngajati.cs
@@ -8,6 +8,47 @@ namespace Clasa_Angajat
 {
     class ListaAngajati
     {
+        Angajat[] a;
 
+        public ListaAngajati()
+        {
+            a = new Angajat[0];
+        }
+        public void Add(Angajat add)
+        {
+            Angajat[] t = new Angajat[a.Length + 1];
+            for (int i = 0; i < a.Length; i++)
+            {
+                t[i] = a[i];
+            }
+            t[a.Length] = add;
+            a = t;
+        }
+         
+        public void Remove(string remove)
+        {
+            int idx = -1;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i].nume == remove)
+                {
+                    idx = i;
+                    break;
+                }
+            }
+            if (idx != -1)
+            {
+                Angajat[] t = new Angajat[a.Length - 1];
+                for (int i = 0; i < idx; i++)
+                {
+                    t[i] = a[i];
+                }
+                for (int i = idx + 1; i < a.Length; i++)
+                {
+                    t[i - 1] = a[i];
+                }
+                a = t;
+            }
+        }
     }
 }
